d2a11aebc895a7d5151f602bd575893ee04da13e
KrieqqeR <burakcevizli@gmail.com>
2023-12-12 16:59:24 +0300

    multiple component ...

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,26 @@
 <template>
- <h1>{{ title }}</h1>
- <input type="text" ref="name">
- <button @click="handleClick">Click me</button>
+  <h1>{{ title }}</h1>
+  <MyModal />
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import MyModal from "./components/MyModal.vue";
+
 
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  components: { MyModal },
+  data() {
     return {
-      title:"My First Vue App"
-    }
+      title: "My First Vue App",
+    };
   },
-  methods:{
-    handleClick(){
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
-    }
-  }
-}
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1{
-  display: inline-block;
-}
-</style>
